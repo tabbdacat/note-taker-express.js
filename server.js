e663a8da4dc5e9990fb247b4bf175f4d0ddc5c56
Meow <tabbathapischke@gmail.com>
2023-10-25 15:59:09 -1000

    change route to htmls, add post route, give it an id

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const db = require('./db/db.json')
+const db = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// creates routes for everything in public folder
 app.use(express.static('public'));
 
 // app.use('/api', api);
@@ -25,9 +28,42 @@ app.get('/notes', (req, res) =>
 );
 
 // route for a GET request that will return the content of `db.json` file
-app.get('/api', (req, res) =>
+app.get('/api/notes', (req, res) =>
+
   res.json(db)
 );
+
+// POST request
+app.post('/api/notes', (req, res) => {
+    req.body.id = uuidv4();
+    console.log(req.body);
+    db.push(req.body);
+    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+
+        return res.status(200).json(req.body);
+    })
+
+    // Let the client know that their POST request was received
+    // res.json(`${req.method} request received`);
+  
+    // // Log our request to the terminal
+    // console.info(`${req.method} request received`);
+
+//     const { title, text } = req.body;
+
+//   // If all the required properties are present
+//   if (title && text) {
+//     // Variable for the object we will save
+//     const newNote = {
+//       title,
+//       text
+//     }
+  });
+
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
